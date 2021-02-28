1e35fe9ffedb16db901d5542022473a1230d5391
Ondrej Hanilec <ohanilec@gmail.com>
2021-02-28 14:10:21 +0100

    API setup 2

--- src/TaskAPI/RestApiClient.java
+++ src/TaskAPI/RestApiClient.java
@@ -13,70 +13,21 @@ public class RestApiClient {
 
     public static void main(String[] args) throws IOException{
 
+        String inputJSON = "{\"name\": \"Jed I. Knight\",\"birthYear\": \"2563\",\"about\": \"The force is strong with this one\"}";
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to the Person Info Command Line Editor.");
-        System.out.println("(PICLER for short.)");
-        System.out.println("Do you want to get or set a person's info?");
-        System.out.println("(Type 'get' or 'set' now.)");
-        String getOrSet = scanner.nextLine();
-        if("get".equalsIgnoreCase(getOrSet)){
-            System.out.println("Whose info do you want to get?");
-            System.out.println("(Type a person's name now.)");
-            String name = scanner.nextLine();
-
-            String jsonString = getPersonData(name);
-            JSONObject jsonObject = new JSONObject(jsonString);
-
-            int birthYear = jsonObject.getInt("birthYear");
-            System.out.println(name + " was born in " + birthYear + ".");
-
-            String about = jsonObject.getString("about");
-            System.out.println(about);
-        }
-        else if("set".equalsIgnoreCase(getOrSet)){
-            System.out.println("Whose info do you want to set?");
-            System.out.println("(Type a person's name now.)");
-            String name = scanner.nextLine();
-
-            System.out.println("When was " + name + " born?");
-            System.out.println("(Type a year now.)");
-            String birthYear = scanner.nextLine();
-
-            System.out.println("Can you tell me about " + name + "?");
-            System.out.println("(Type a sentence now.)");
-            String about = scanner.nextLine();
-
-            setPersonData(name, birthYear, about);
-        }
-
+        System.out.println("input JSON string");
+        String jsonString = inputJSON;
+        System.out.println(jsonString);
+        JSONObject jsonObject = new JSONObject(jsonString);
         scanner.close();
 
-        System.out.println("Thanks for using PICLER.");
+        String name = jsonObject.getString("name");
+        String birthYear = jsonObject.getString("birthYear");
+        String about = jsonObject.getString("about");
+        System.out.println(name + " " + birthYear + " " + about);
 
-    }
-
-    public static String getPersonData(String name) throws IOException{
-
-        HttpURLConnection connection = (HttpURLConnection) new URL("http://localhost:9200/people/" + name).openConnection();
-
-        connection.setRequestMethod("GET");
-
-        int responseCode = connection.getResponseCode();
-        if(responseCode == 200){
-            String response = "";
-            Scanner scanner = new Scanner(connection.getInputStream());
-            while(scanner.hasNextLine()){
-                response += scanner.nextLine();
-                response += "\n";
-            }
-            scanner.close();
-
-            return response;
-        }
-
-        // an error happened
-        return null;
+        setPersonData(name, birthYear, about );
     }
 
     public static void setPersonData(String name, String birthYear, String about) throws IOException{
@@ -84,8 +35,8 @@ public class RestApiClient {
 
         connection.setRequestMethod("POST");
 
-        String postData = "name=" + URLEncoder.encode(name);
-        postData += "&about=" + URLEncoder.encode(about);
+        String postData = "name=" + URLEncoder.encode(name, "UTF-8");
+        postData += "&about=" + URLEncoder.encode(about, "UTF-8");
         postData += "&birthYear=" + birthYear;
 
         connection.setDoOutput(true);
@@ -99,6 +50,9 @@ public class RestApiClient {
         }
         else if(responseCode == 401){
             System.out.println("Wrong password.");
+        }
+        else{
+            System.out.println("Just wrong");
         }
     }
 }
